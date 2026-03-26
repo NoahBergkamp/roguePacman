96ec24af34221d7e44b1f60037bcded134348e8d
noah bergkamp <59866@ma-web.nl>
2026-03-26 11:19:53 +0100

    PointSystemEnemy

---
+++ Assets/Scripts/EnemyPointSystem.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyPointSystem : MonoBehaviour
+{
+    public float speed = 2f;
+    public Transform[] points;
+    public SpriteRenderer spriteRenderer;
+
+    private int i;
+    void Start()
+    {
+        transform.position = points[0].position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards( transform.position, points[i].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, points[i].position) < 0.01f)
+        {
+            i++;
+            if (i == points.Length)
+            {
+                i = 0;
+            }
+
+        }
+
+        spriteRenderer.flipX = (transform.position.x - points[i].position.x) < 0f;
+    }
+}
